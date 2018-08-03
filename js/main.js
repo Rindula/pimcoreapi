3d2d03469f906dab6046411c6d5aac01cda8a7ec
Rindula <contact@rindula.de>
2018-08-03 08:03:06 +0200

    Aufgeräumt

--- js/main.js
+++ js/main.js
@@ -2,13 +2,3 @@ function save(e) {
     if (e.value == $( e ).attr( "data-prevalue")) return;
     $(e).parents().filter("form").submit();
 }
-
-function transformTag(tagIdOrElem, tagType){
-    var elem = (tagIdOrElem instanceof HTMLElement) ? tagIdOrElem : document.getElementById(tagIdOrElem);
-    if(!elem || !(elem instanceof HTMLElement))return;
-    var parent = elem.parentNode;
-    var newNode = document.createElement(tagType||"span");
-    parent.replaceChild(newNode,elem);
-
-    return newNode;
-}
